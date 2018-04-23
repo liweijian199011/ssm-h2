943621f0682525fc04f611f135b1e43aa54c0948
xuansheng.lwj <xuansheng.lwj@alibaba-inc.com>
2018-04-23 10:25:08 +0800

    Initial commit

---
+++ src/main/java/com/jack/ssmh2/SsmH2Application.java
@@ -0,0 +1,12 @@
+package com.jack.ssmh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmH2Application.class, args);
+	}
+}
