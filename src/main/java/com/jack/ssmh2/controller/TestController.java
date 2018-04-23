af993536dc1ece86d7ee6f2f5ac2e81cfecf55aa
xuansheng.lwj <xuansheng.lwj@alibaba-inc.com>
2018-04-23 11:14:13 +0800

    test

--- src/main/java/com/jack/ssmh2/controller/TestController.java
+++ src/main/java/com/jack/ssmh2/controller/TestController.java
@@ -1,5 +1,13 @@
 package com.jack.ssmh2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
 
+    @GetMapping("/test")
+    public String test() {
+        return "hello, world!";
+    }
 }
